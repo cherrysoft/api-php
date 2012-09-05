d0bb9b7685287f0bfee77bd18529e06fe80b8d8f
Luis E Fabian <luis@ordrin.com>
2012-09-05 11:44:37 -0400

    Address.php

--- Ordrin/Address.php
+++ Ordrin/Address.php
@@ -14,28 +14,17 @@ class Address {
     }
 
     function validate($element = "all") {
-      $_errors = array();
-
-      // do ALL validation
-      if (!preg_match('/(^\d{5}$)|(^\d{5}\-\d{4}$)/', $this->zip)) {
-        $_errors[] = 'Address - Validation - zip code (invalid) (' . $this->zip . ')';
-      }
-
-      if (!preg_match('/^\(?\d{3}\)?[\- .]?\d{3}[\- .]?\d{4}$/', $this->phone)) {
-        $_errors[] = 'Address - Validation - phone number (invalid, must be in format ###-###-####) (' . $this->phone . ')';
-      }
-
-      if (!preg_match('/^[A-z.\- ]+$/', $this->city)) {
-        $_errors[] = 'Address - Validation - city (invalid, only letters/spaces allowed) (' . $this->city . ')';
-      }
-            
-      if (!preg_match('/^[a-z]{2}$/i', $this->state)) {
-        $_errors[] = 'Address - validation - state (invalid, must be two-letter state abbreviation) (' . $this->state . ')';
-      }
-
-      if(!empty($_errors)) {
-        throw new OrdrinExceptionBadValue($_errors);
-      }
+    	
+    	$_errors = array();
+    	$validation = new Validation();
+     	//do ALL validation
+      	$validation -> validateZipCode($this->zip);
+      	$validation -> validatePhone($this->phone);
+      	$validation -> validateCity($this->city);
+      	$validation -> validateState($this->state);
+     	if(!empty($validation->errors)) {
+     	   throw new OrdrinExceptionBadValue($validation->errors);
+      	}
     }
 
 
